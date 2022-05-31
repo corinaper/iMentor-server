0b31d2ad747ee6675eb7dc909e074bc80c1170f1
Corina Perjan <cperjan@gmail.com>
2022-05-31 13:25:14 +0200

    fix invertd piramid

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-})
-
 router.use("/coasters", require('./coaster.routes'))
 
 router.use("/auth", require('./auth.routes'))
 
 router.use("/upload", require('./upload.routes'))
+
+router.get("/", (req, res, next) => {
+  res.json("All good in here");
+})
 
 module.exports = router
